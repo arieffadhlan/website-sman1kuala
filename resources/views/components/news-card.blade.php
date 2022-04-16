a1677ac2ddad787c00410dc1f594ebd01e37e6f4
Muhammad Arief Fadhlan <ariffadhlan12@gmail.com>
2022-04-16 17:21:35 +0700

    (homepage): fix news and ekstrakulikuler card shadow

--- resources/views/components/news-card.blade.php
+++ resources/views/components/news-card.blade.php
@@ -1,5 +1,5 @@
-<div class="flex flex-col max-w-[24rem] w-full rounded-lg shadow-lg">
-    <div class="h-44 bg-[#BCD1FF] rounded-t-lg"></div>
+<div class="flex flex-col max-w-[24rem] w-full rounded-lg shadow-md">
+    <div class="h-44 bg-secondary rounded-t-lg"></div>
     <div class="px-6 py-4">
         <div class="flex justify-between items-center mb-3">
             <span class="font-semibold text-xs text-neutral">#akademik</span>
@@ -13,7 +13,7 @@
         </div>
         <div class="flex justify-between items-center">
             <div class="flex items-center gap-x-2 w-full">
-                <div class="w-8 h-8 rounded-full bg-[#BCD1FF]"></div>
+                <div class="w-8 h-8 rounded-full bg-secondary"></div>
                 <span class="font-semibold text-xs text-primary">Admin</span>
             </div>
             <span class="font-semibold text-xs text-primary">Baca</span>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -17,7 +17,7 @@
     </x-container>
     <div class="flex flex-col justify-center items-center my-[60px] py-[60px] bg-[#DCF0E2]">
         <h1 class="mb-[64px] relative text-center font-bold text-3xl tracking-[-0.021em] text-primary after:content-[' '] after:w-[120px] after:h-1 after:left-1/2 after:bottom-[-12px] after:absolute after:bg-secondary after:-translate-x-1/2">Ekstrakulikuler</h1>
-        <div class="grid grid-rows-2 grid-cols-4 place-items-center gap-7">
+        <div class="grid grid-rows-2 grid-cols-4 place-items-center gap-5">
             <x-ekstrakulikuler-card></x-ekstrakulikuler-card>
         </div>
     </div>

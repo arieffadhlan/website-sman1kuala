fb7c57017ab4049f9198cb8f0f2ee40270885735
Muhammad Arief Fadhlan <ariffadhlan12@gmail.com>
2022-05-29 19:10:03 +0700

    Update sambutan kepala sekolah and sejarah

--- resources/views/pages/homepage/profil/sambutan-kepala-sekolah.blade.php
+++ resources/views/pages/homepage/profil/sambutan-kepala-sekolah.blade.php
@@ -5,31 +5,16 @@
             <h1 class="mb-9 pb-4 border-b border-neutral border-opacity-20 text-3xl leading-[42px] traking-[-0.021em] font-bold text-primary sm:text-4xl sm:leading-[48px]">Kata Sambutan Kepala Sekolah SMA Negeri 9 Medan</h1>
         </div>
         <div class="flex flex-col gap-y-5 w-full text-justify">
-            <p class="text-base tracking-[-0.011em] font-normal text-primary">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p class="text-base tracking-[-0.011em] font-normal text-primary sm:text-lg sm:tracking-[-0.014em]">
+                Puji syukur kami panjatkan kehadirat Tuhan yang Maha Esa atas limpahan rahmat dan karunia NYA sehingga akhirnya dapat meluncurkan Website SMAN 9 Medan. Kehadiran Website SMAN 9 Medan diharapkan dapat memudahkan penyampaian informasi secara terbuka kepada warga sekolah, alumni dan masyarakat serta instansi lain yang terkait. 
+                Semoga dengan langkah ini kami dapat meningkatkan pelayanan kepada masyarakat khususnya orang tua peserta didik SMAN 9 Medan serta meningkatkan kualitas hasil  pembelajaran (output) yang lebih berbobot sebagai generasi penerus bangsa sejalan dengan Visi Misi yang telah ditetapkan. Selamat bekerja dan terima kasih.
             </p>
-            <p class="text-base tracking-[-0.011em] font-normal text-primary">    
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <p class="text-base tracking-[-0.011em] font-normal text-primary">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+            <p class="text-right text-base tracking-[-0.011em] font-normal text-primary sm:text-lg sm:tracking-[-0.014em]">
+                <span>Medan , 27 Mei 2022</span>
+                <div class="flex flex-col justify-end items-end gap-y-10 text-base tracking-[-0.011em] font-normal text-primary sm:text-lg sm:tracking-[-0.014em]">
+                    <span>Kepala Sekolah SMAN 9 Medan</span>
+                    <span>Riko Marbun SPd, MSi</span>
+                </div>
             </p>
         </div>
     </x-container>
